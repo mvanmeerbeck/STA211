5f93eba39a7b3a86c704bcaf7c4ca8db9e75e42a
Maxime Vanmeerbeck <mvanmeerbeck@shopbot-inc.com>
2018-07-08 18:23:09 +0200

    font

--- plots/validationCurve.py
+++ plots/validationCurve.py
@@ -6,6 +6,10 @@ import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.svm import SVC
 from sklearn.model_selection import validation_curve
+from matplotlib import rcParams
+
+rcParams['font.family'] = 'EB Garamond'
+rcParams['font.size'] = '13'
 
 digits = load_digits()
 X, y = digits.data, digits.target
@@ -19,17 +23,17 @@ train_scores_std = np.std(train_scores, axis=1)
 test_scores_mean = np.mean(test_scores, axis=1)
 test_scores_std = np.std(test_scores, axis=1)
 
-plt.title("Validation Curve with SVM")
+plt.title("Courbe de validation avec SVM")
 plt.xlabel("$\gamma$")
 plt.ylabel("Score")
 plt.ylim(0.0, 1.1)
 lw = 2
-plt.semilogx(param_range, train_scores_mean, label="Training score",
+plt.semilogx(param_range, train_scores_mean, label="Score apprentissage",
              color="darkorange", lw=lw)
 plt.fill_between(param_range, train_scores_mean - train_scores_std,
                  train_scores_mean + train_scores_std, alpha=0.2,
                  color="darkorange", lw=lw)
-plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+plt.semilogx(param_range, test_scores_mean, label="Score validation croisee",
              color="navy", lw=lw)
 plt.fill_between(param_range, test_scores_mean - test_scores_std,
                  test_scores_mean + test_scores_std, alpha=0.2,
